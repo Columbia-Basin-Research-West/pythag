95c111a56f808c1c01d83ed2be4875bf55857faf
swhitCBR <swhit@uw.edu>
2021-09-09 10:53:34 -0700

    initial commit

---
+++ R/hypotenuse.R
@@ -0,0 +1,15 @@
+
+#' @title calculates the hypo.
+#'
+#' @param a side one
+#' @param b side two
+#'
+#' @return The length of the third side.
+#' @export
+#'
+hypotenuse <- function(a,b){
+  c=sqrt(a^2 + b^2)
+  return(c)
+}
+
+
